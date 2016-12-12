4b733a23e1b9989e98a13dd2ec19b5339a37e913
tywalch <tywalch@gmail.com>
2016-12-12 11:41:42 -0800

    implementing better comments

--- MSSQL/billquick/storedprocedures/sp_lockoutdate.sql
+++ MSSQL/billquick/storedprocedures/sp_lockoutdate.sql
@@ -7,19 +7,19 @@ BEGIN
 DECLARE @day_value date, @FirstOfTheMonth date, @SixteenthOfTheMonth date, @FirstPayPeriodEnd date, @SixteenthPayPeriodEnd date
 	DECLARE @day_int int, @lockout_int int, @DaysSinceLastSixteenth int
 	DECLARE @lockout_value nvarchar(10)
-	SET @day_value = GETDATE() --12/11/2016
-	SET @day_int = DAY(@day_value) --11
-	SET @FirstOfTheMonth = DATEFROMPARTS(YEAR(GETDATE()),MONTH(@day_value),1) --12/01/2016
-	SET @SixteenthOfTheMonth = DATEFROMPARTS(YEAR(GETDATE()),MONTH(@day_value),16) --12/16/2016
+	SET @day_value = GETDATE()
+	SET @day_int = DAY(@day_value)
+	SET @FirstOfTheMonth = DATEFROMPARTS(YEAR(GETDATE()),MONTH(@day_value),1)
+	SET @SixteenthOfTheMonth = DATEFROMPARTS(YEAR(GETDATE()),MONTH(@day_value),16)
 	SET @DaysSinceLastSixteenth = DATEDIFF(dd, DATEADD(mm, -1, (DATEFROMPARTS(DATEPART(yyyy, @day_value), DATEPART(mm, @day_value), 16))), @day_value) --23
 
-	SET @FirstPayPeriodEnd = @FirstOfTheMonth --12/01/2016
+	SET @FirstPayPeriodEnd = @FirstOfTheMonth
 	WHILE dbo.CheckHoliday(@FirstPayPeriodEnd) = 1
 	BEGIN
-		SET @FirstPayPeriodEnd = DATEADD(dd, 1, @FirstPayPeriodEnd) -- 12/1/2016
+		SET @FirstPayPeriodEnd = DATEADD(dd, 1, @FirstPayPeriodEnd)
 	END
 
-	SET @SixteenthPayPeriodEnd = @SixteenthOfTheMonth --12/16/2016
+	SET @SixteenthPayPeriodEnd = @SixteenthOfTheMonth
 	WHILE dbo.CheckHoliday(@SixteenthPayPeriodEnd) = 1
 	BEGIN
 		SET @SixteenthPayPeriodEnd = DATEADD(dd, 1, @SixteenthPayPeriodEnd)
@@ -46,7 +46,7 @@ DECLARE @day_value date, @FirstOfTheMonth date, @SixteenthOfTheMonth date, @Firs
 	and @day_int > 1
 		SET @lockout_int = @day_int - 1
 
-	-- give access back to the first of the month	
+	-- give access back to the first of the month
 	ELSE IF @day_int >= 16
 		SET @lockout_int = 1
 
